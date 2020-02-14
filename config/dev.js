53bea3301d4ef6571990f268197bfb8c88a3b971
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-14 11:32:53 -0500

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI:
+    'mongodb+srv://dbUser:testpass123@cluster0-ak2zi.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: '123123123'
+};
