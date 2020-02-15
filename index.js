cbfdf9ab2353346006be70323ec61636f230597e
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-15 11:30:50 -0500

    Added data caching with redis

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect(keys.mongoURI, { useMongoClient: true });
+require('./services/cache');
 
 mongoose
   .connect(keys.mongoURI, {
