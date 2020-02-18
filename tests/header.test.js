5e0b700d5df55dc97c0c6763d06895d84bfa843e
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-18 14:09:16 -0500

    Updated setup file and refactored header test adding two factory functions for sessions and users

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactories');
 
 let browser, page;
 
@@ -30,25 +32,11 @@ test('Clicking login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-  const id = '5e4586e9c011dfee7d09cda4';
+  // const id = '5e4586e9c011dfee7d09cda4';
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
 
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    'base64'
-  );
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
-
-  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session', value: session });
   await page.setCookie({ name: 'session.sig', value: sig });
   await page.goto('localhost:3000');
   await page.waitFor('a[href="/auth/logout"]');
--- tests/setup.js
+++ tests/setup.js
@@ -1,1 +1,14 @@
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
 jest.setTimeout(30000);
