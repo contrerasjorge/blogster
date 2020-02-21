bc2415b8bc0b34d95c036cae21b581c80d044d87
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-21 16:30:31 -0500

    Updated submitBlog

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,11 +16,13 @@ export const handleToken = token => async dispatch => {
 export const submitBlog = (values, file, history) => async dispatch => {
   const uploadConfig = await axios.get('/api/upload');
 
-  await axios.put(uploadConfig.data.url, file, {
-    headers: {
-      'Content-Type': file.type
-    }
-  });
+  if (file !== null) {
+    await axios.put(uploadConfig.data.url, file, {
+      headers: {
+        'Content-Type': file.type
+      }
+    });
+  }
 
   const res = await axios.post('/api/blogs', {
     ...values,
